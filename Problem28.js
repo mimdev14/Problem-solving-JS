1aa3f371259af0ca371bab50ad2c5f12128e4d01
MIM-0014 <fatemaaktermim1031@gmail.com>
2026-09-21 15:35:59 +0600

    Promise Chain

---
+++ Problem28.js
@@ -0,0 +1,14 @@
+function delay(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+
+delay(1000)
+  .then(() => {
+    console.log('1 sec');
+    return delay(2000);
+  })
+  .then(() => {
+    console.log('3 sec total');
+  });
