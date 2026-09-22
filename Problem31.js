bc8092b456cc645150bf330f230e7d0f1163a6e3
MIM-0014 <fatemaaktermim1031@gmail.com>
2026-09-23 00:01:48 +0600

    Debounce Function

---
+++ Problem31.js
@@ -0,0 +1,11 @@
+function debounce(fn, delay) {
+  let timer;
+
+  return function (...args) {
+    clearTimeout(timer);
+
+    timer = setTimeout(() => {
+      fn(...args);
+    }, delay);
+  };
+}
