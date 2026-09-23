ba61f903940043405cc1d3c9612b74a2d6ba3297
MIM-0014 <fatemaaktermim1031@gmail.com>
2026-09-23 12:45:41 +0600

    Celsius to Fahrenheit

---
+++ Problem4.js
@@ -0,0 +1,7 @@
+function toFahrenheit(celsius) {
+  return (celsius * 9 / 5) + 32;
+}
+
+console.log(toFahrenheit(0));   // 32
+console.log(toFahrenheit(100)); // 212
+console.log(toFahrenheit(25));  // 77
